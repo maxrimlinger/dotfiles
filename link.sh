f796f63e67ae87e9c6681d6876c2079e04ba99e6
Max Rimlinger <37249275+maxrimlinger@users.noreply.github.com>
2025-02-27 13:30:01 -0500

    Fix linking and remove recursive symlink

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.pycodestyle ~/.config/pycodestyle
+ln -sf ~/dotfiles/pycodestyle ~/.config/pycodestyle
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
