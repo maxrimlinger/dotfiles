9833f56d2a088c4409c7d47e83f8bf037f93243f
Willow Rimlinger <wrimlinger@d3embedded.com>
2025-10-03 15:30:23 -0400

    Have ctrl-j and k in normal mode in Telescope nvim

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -22,6 +22,8 @@ return {
             ["<C-q>"] = actions.send_to_qflist,
           },
           n = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-c>"] = actions.close,
             ["q"] = actions.close,
           },
